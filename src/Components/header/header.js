a9120de24ef8d50757a24f3f9de571b7c44d34c9
DocQuanty <Urchikak8@gmail.com>
2021-09-21 16:09:10 +0300

    [header] create component

--- src/Components/header/header.js
+++ src/Components/header/header.js
@@ -1,28 +1,32 @@
 import s from "../header/header.module.scss";
+import { Route } from "react-router-dom";
 
 export const Header = (props) => {
   return (
     <div className={s.header}>
-      <ul>
-        <li>
-          <a href="#">Головна</a>
-        </li>
-        <li>
-          <a href="#">Буковель </a>
-        </li>
-        <li>
-          <a href="#">Яремче</a>
-        </li>
-        <li>
-          <a href="#">Котеджі</a>
-        </li>
-        <li>
-          <a href="#">Chalet</a>
-        </li>
-        <li>
-          <a href="#">Контакти</a>
-        </li>
-      </ul>
+      {/* ======== */}
+      <div className={s.nav}>
+        <ul>
+          <li>
+            <a href="/mainPage">Головна</a>
+          </li>
+          <li>
+            <a href="/bukovelPage">Буковель </a>
+          </li>
+          <li>
+            <a href="/yaremche">Яремче</a>
+          </li>
+          <li>
+            <a href="/Cottages">Котеджі</a>
+          </li>
+          <li>
+            <a href="/contact-us">Контакти</a>
+          </li>
+        </ul>
+      </div>
+      <div className={s.tel_number}>
+        <p>+38 (096) 740 4337</p>
+      </div>
     </div>
   );
 };
