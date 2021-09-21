0dfcdf8adbfa147b2fe3a3a05c9a265f36d07bd1
DocQuanty <Urchikak8@gmail.com>
2021-09-21 19:42:33 +0300

    create component

--- src/Components/Page/headerPage.js
+++ src/Components/Page/headerPage.js
@@ -1,9 +1,11 @@
 import { Header } from "../header/header";
+import { QuestionSearch } from "../questionSearch/questionSearch";
 
 const headerPage = () => {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <QuestionSearch />
     </>
   );
 };
---
+++ src/Components/questionSearch/questionSearch.js
@@ -0,0 +1,5 @@
+import s from "../questionSearch/questionSearch.module.scss";
+
+export const QuestionSearch = () => {
+  return <div className={s.questionSearch}>questionSearch</div>;
+};
