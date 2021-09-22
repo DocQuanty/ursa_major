246cf83bca0304cf9fdeabeb65e9c5f483229310
DocQuanty <Urchikak8@gmail.com>
2021-09-22 18:33:38 +0300

    [MainTitle] del theem tag

--- src/Components/mainTitle/mainTitle.js
+++ src/Components/mainTitle/mainTitle.js
@@ -4,10 +4,6 @@ export const MainTitle = () => {
   return (
     <div className={s.main_title}>
       <div className={s.main_big_title}>Велика Ведмедиця</div>
-
-      <div className={s.main_big_teg}>
-        Мережа готелів та котеджів в Буковелі та Яремче
-      </div>
     </div>
   );
 };
