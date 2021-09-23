d10b2a078b4fe4083538495959e7024932f69444
DocQuanty <Urchikak8@gmail.com>
2021-09-23 14:48:39 +0300

    create sideText [SideText/SideText.module] /[index] fix margin box-sizing/[font.scss]add font  Lora

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import HeaderPage from "./Components/Page/headerPage";
+// import HeaderPage from "./Components/Page/headerPage";
 import "./fonts/fonts.scss";
 import "./App.scss";
+import SideText from "./Components/SideText/SideText";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SideText titleName={"Велика ведмедиця"} mode={"modeWhite"}>
+        <p>
+          Отримайте комплексний відпочинок, а не ключі від номеру. <br />
+          <span style={{ color: "orange" }}>
+            У нас європейський сервіс та домашня атмосфера затишку і
+            гостинності.
+          </span>
+        </p>
+        <ul>
+          <li>Готельно-ресторанний комплекс "Ведмежий Двір" у Буковелі</li>
+          <li>Family готель в Яремче</li>
+          <li>Елітні котеджі від Яремча до Буковеля</li>
+        </ul>
+      </SideText>
+    </div>
+  );
 }
 
 export default App;
--- src/Components/SideText/SideText.js
+++ src/Components/SideText/SideText.js
@@ -1,7 +1,44 @@
 import s from "../SideText/sideText.module.scss";
+import TheemTag from "../TheemTag/TheemTag";
 
 export const SideText = (props) => {
-  return <div className={s.sideText}>123</div>;
+  const title = () => {
+    if (props.mode === "modeWhite") {
+      return <h2 className={s.sideText_titleWhite}>{props.titleName}</h2>;
+    } else if (props.mode === "modeBlack") {
+      return <h2 className={s.sideText_titleBlack}>{props.titleName}</h2>;
+    } else {
+      return null;
+    }
+  };
+  const content = () => {
+    if (props.mode === "modeWhite") {
+      return (
+        <div style={{ color: "#fff" }} className={s.sideText_content}>
+          {props.children}
+        </div>
+      );
+    } else if (props.mode === "modeBlack") {
+      return (
+        <div style={{ color: "#000" }} className={s.sideText_content}>
+          {props.children}
+        </div>
+      );
+    }
+  };
+
+  return (
+    <>
+      <div className={s.sideText}>
+        <div className={s.sideText_tag}>
+          <TheemTag />
+        </div>
+        {title()}
+        {content()}
+        <div className={s.sideText_link}></div>
+      </div>
+    </>
+  );
 };
 
 export default SideText;
