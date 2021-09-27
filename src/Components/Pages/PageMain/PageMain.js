b780dd5ab948accbed77b9e067aea8d3d6597818
DocQuanty <Urchikak8@gmail.com>
2021-09-28 00:09:24 +0300

    add main page

--- src/Components/Pages/PageMain/PageMain.js
+++ src/Components/Pages/PageMain/PageMain.js
@@ -1,30 +1,40 @@
 import s from "./PageMain.module.scss";
 import SlideText from "../../SideText/SideText";
+// import TheemTag from "../TheemTag/TheemTag";
 import TwoLink from "../../SideText/TwoLink/TwoLinks";
+import { Header } from "../../header/header";
 
 const PageMain = (props) => {
   return (
     <div className={s.PageMain}>
-      <SlideText
-        titleMain={"Велика ведмедиця"}
-        marginBottom={20}
-        arr={props.state}
-        mode={"modeWhite"}
-      >
-        <p>
-          Отримайте комплексний відпочинок, а не ключі від номеру. <br />
-          <span style={{ color: "orange" }}>
-            У нас європейський сервіс та домашня атмосфера затишку і
-            гостинності.
-          </span>
-        </p>
-        <ul>
-          <li>Готельно-ресторанний комплекс "Ведмежий Двір" у Буковелі</li>
-          <li>Family готель в Яремче</li>
-          <li>Елітні котеджі від Яремча до Буковеля</li>
-        </ul>
-      </SlideText>
-      <TwoLink links={props.state.twoLinks}></TwoLink>
+      <div className={s.PageMain_wrapper}>
+        <div className={s.PageMain__header}>
+          <Header />
+        </div>
+        <div className={s.PageMain__SlideText}>
+          {/* <TheemTag /> */}
+          <SlideText
+            titleMain={"Велика ведмедиця"}
+            marginBottom={20}
+            arr={props.state}
+            mode={"modeWhite"}
+          >
+            <p>
+              Отримайте комплексний відпочинок, а не ключі від номеру. <br />
+              <span style={{ color: "orange" }}>
+                У нас європейський сервіс та домашня атмосфера затишку і
+                гостинності.
+              </span>
+            </p>
+            <ul>
+              <li>Готельно-ресторанний комплекс "Ведмежий Двір" у Буковелі</li>
+              <li>Family готель в Яремче</li>
+              <li>Елітні котеджі від Яремча до Буковеля</li>
+            </ul>
+          </SlideText>
+          <TwoLink links={props.state.twoLinks}></TwoLink>
+        </div>
+      </div>
     </div>
   );
 };
